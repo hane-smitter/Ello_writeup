9846c8f717c6840fd8e26e7077a391dcbaa03db3
zaki <hanesmitter3@gmail.com>
2024-06-12 15:40:25 +0300

    Include assets

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,6 @@
+function Footer() {
+  return <div>This is footer cmp</div>;
+}
+
+export default Footer;
+
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>This is header cmp</div>;
+}
+
+export default Header;
